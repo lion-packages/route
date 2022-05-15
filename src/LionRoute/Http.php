f9aff489ef2a6be5a2c32778b600314406482981
Sergio León <sergioleon4004@hotmail.com>
2022-05-15 09:22:25 -0500

    Feat(Http): New type of http request 'options' has been integrated

--- src/LionRoute/Http.php
+++ src/LionRoute/Http.php
@@ -84,4 +84,16 @@ class Http {
 		}
 	}
 
+	public static function options(string $url, \Closure|array $controller_function, array $filters = []): void {
+		if (count($filters) > 0) {
+			self::$router->options(
+				$url,
+				$controller_function,
+				isset($filters[1]) ? ['before' => $filters[0], 'after' => $filters[1]] : ['before' => $filters[0]]
+			);
+		} else {
+			self::$router->options($url, $controller_function);
+		}
+	}
+
 }

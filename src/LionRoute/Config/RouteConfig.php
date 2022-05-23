40d06995297ddec4b200d8ca673e1984289fa43e
Sergio León <sergioleon4004@hotmail.com>
2022-05-23 09:06:52 -0500

    Refactor(RouteConfig): Added url fetching dynamically

--- src/LionRoute/Config/RouteConfig.php
+++ src/LionRoute/Config/RouteConfig.php
@@ -9,6 +9,10 @@ class RouteConfig {
 	}
 
 	public static function processInput(int $index): string {
+		if ($index === 1) {
+			return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		}
+
 		return implode('/', array_slice(explode('/', $_SERVER['REQUEST_URI']), $index));
 	}
 
